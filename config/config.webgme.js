35209b6ecc520681ba7d89afc93886208bd08711
Sanjana Das <sanjanadas@toors-MacBook-Pro.local>
2023-12-03 16:19:58 -0600

    new seed created

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -9,6 +9,7 @@ var config = require('webgme/config/config.default'),
 // The paths can be loaded from the webgme-setup.json
 config.plugin.basePaths.push(__dirname + '/../src/plugins');
 config.seedProjects.basePaths.push(__dirname + '/../src/seeds/othello_game');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/othello_G');
 
 
 
