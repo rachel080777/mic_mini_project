61c0fe0a9e5ac233dd6f3f16b0c0f30a82adc2ed
Sanjana Das <sanjanadas@toors-MacBook-Pro.local>
2023-12-03 00:31:53 -0600

    Added first plugin

--- config/config.default.js
+++ config/config.default.js
@@ -5,7 +5,9 @@ var config = require('./config.webgme'),
 
 // Add/overwrite any additional settings here
 // config.server.port = 8080;
-// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+//config.mongo.uri = 'mongodb://127.0.0.1:27017/sanjanadb';
+config.plugin.allowServerExecution=true;
 
 validateConfig(config);
 module.exports = config;
+
